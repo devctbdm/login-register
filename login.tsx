04631f1eb3a693f4e822aacef9287804ef2d5e55
devctbdm <devctme@gmail.com>
2026-02-28 10:02:54 +0600

    Update login.tsx
    login user shadcn ui zod validation

--- login.tsx
+++ login.tsx
@@ -1,1 +1,161 @@
 
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  Field,
+  FieldError,
+  FieldGroup,
+  FieldLabel,
+} from "@/components/ui/field";
+import { Input } from "@/components/ui/input";
+import { zodResolver } from "@hookform/resolvers/zod";
+import axios from "axios";
+import { Eye, EyeOff } from "lucide-react";
+import { useState } from "react";
+import { Controller, useForm } from "react-hook-form";
+import { toast } from "sonner";
+import { z } from "zod";
+
+const formSchema = z.object({
+  email: z.email("Enter a valid email address."),
+  password: z
+    .string()
+    .min(6, "Password must be at least 6 characters.")
+    .max(32, "Password must be at most 32 characters."),
+});
+
+interface TokenProps {
+  setToken: (token: string | null) => void;
+}
+
+const backendUrl = import.meta.env.VITE_BACKEND_URL;
+
+const Login = ({ setToken }: TokenProps) => {
+  const [showPassword, setShowPassword] = useState<boolean>(false);
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  const onSubmit = async (data: z.infer<typeof formSchema>) => {
+    try {
+      const response = await axios.post(`${backendUrl}/api/user/admin`, data);
+
+      if (response.data.token) {
+        setToken(response.data.token);
+        toast.success(response.data.message);
+      } else {
+        toast.error(response.data.message);
+      }
+    } catch (error) {
+      console.log("Admin login error", error);
+      toast.error("Admin login error");
+    } finally {
+      form.reset();
+    }
+  };
+
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <Card className="w-[400px]">
+        <CardHeader className="text-center">
+          <CardTitle className="text-2xl font-bold">Login User</CardTitle>
+          <CardDescription className="text-sm text-gray-500">
+            Login to your account
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form id="form-rhf-demo" onSubmit={form.handleSubmit(onSubmit)}>
+            <FieldGroup>
+              <Controller
+                name="email"
+                control={form.control}
+                render={({ field, fieldState }) => (
+                  <Field data-invalid={fieldState.invalid}>
+                    <FieldLabel htmlFor="email" className="cursor-pointer mt-2">
+                      Email
+                    </FieldLabel>
+                    <Input
+                      type="email"
+                      {...field}
+                      id="email"
+                      aria-invalid={fieldState.invalid}
+                      placeholder="Enter your email"
+                      autoComplete="off"
+                    />
+                    {fieldState.invalid && (
+                      <FieldError errors={[fieldState.error]} />
+                    )}
+                  </Field>
+                )}
+              />
+            </FieldGroup>
+            <FieldGroup>
+              <Controller
+                name="password"
+                control={form.control}
+                render={({ field, fieldState }) => (
+                  <Field data-invalid={fieldState.invalid}>
+                    <FieldLabel
+                      htmlFor="password"
+                      className="cursor-pointer mt-2"
+                    >
+                      Password
+                    </FieldLabel>
+                    <div className="relative">
+                      <Input
+                        type={showPassword ? "text" : "password"}
+                        {...field}
+                        id="password"
+                        aria-invalid={fieldState.invalid}
+                        placeholder="Enter your password"
+                        autoComplete="off"
+                        className="pr-10"
+                      />
+                      <Button
+                        type="button"
+                        variant="ghost"
+                        size="icon"
+                        className="absolute right-0 top-0 h-full w-10 hover:bg-transparent"
+                        onClick={() => setShowPassword(!showPassword)}
+                      >
+                        {showPassword ? (
+                          <EyeOff className="h-4 w-4" />
+                        ) : (
+                          <Eye className="h-4 w-4" />
+                        )}
+                      </Button>
+                    </div>
+                    {fieldState.invalid && (
+                      <FieldError errors={[fieldState.error]} />
+                    )}
+                  </Field>
+                )}
+              />
+            </FieldGroup>
+          </form>
+        </CardContent>
+        <CardFooter>
+          <Field orientation="horizontal">
+            <Button type="submit" form="form-rhf-demo" className="w-full">
+              Login
+            </Button>
+          </Field>
+        </CardFooter>
+      </Card>
+    </div>
+  );
+};
+
+export default Login;
